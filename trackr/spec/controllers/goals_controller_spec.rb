664ac834e37c5a125968ad15063578da6d49da68
Margie Lehmann <mlehman1@binghamton.edu>
2017-11-14 01:50:44 +0000

    fixed rspec update test, correct now

--- trackr/spec/controllers/goals_controller_spec.rb
+++ trackr/spec/controllers/goals_controller_spec.rb
@@ -18,14 +18,16 @@ describe GoalsController, type: :controller do
       get :new
       expect(response).to render_template('new')
     end
+    
+    
   end
   
   describe 'PUT #update' do
     describe 'When updating the users goal,' do
-      let!(:testgoal) {FactoryGirl.create(:goal)}
+      let!(:goal) { FactoryGirl.create(:goal) }
       
       it "it uses controller update method with new value as parameter" do
-        put :update, id: 0, goal: FactoryGirl.attributes_for(:goal, goal: 90)
+        put :update, id: 1, goal: FactoryGirl.attributes_for(:goal, goal: 90)
         expect(flash[:notice]).to be_present
       end
       
@@ -34,8 +36,8 @@ describe GoalsController, type: :controller do
       end
       
       it 'updates goal with value 90' do
-        testgoal.reload
-        expect(testgoal.goal).to eql(90)
+        goal.reload
+        expect(Goal.find(1).goal).to eql(90)
       end 
     end
   end
